e1a143c3fa9089ba045ad2b80dd7e2a2610ac048
mansur <kurtov.mansur@gmail.com>
2021-06-07 16:26:27 +0500

    Add project files.

---
+++ AddDigits/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace AddDigits
+{
+    /// <summary>
+    /// Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prg = new Program();
+            var result1 = prg.AddDigits(38); //2
+            var result2 = prg.AddDigits(0); //0
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public int AddDigits(int num)
+        {
+            return GetDightsSum(num);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        private int GetDightsSum(int num)
+        {
+            var s = 0;
+            while (num > 0)
+            {
+                var a = num % 10;
+                s += a;
+                num = num / 10;
+            }
+            if (s < 10)
+                return s;
+
+            return GetDightsSum(s);
+        }
+    }
+
+    
+}
